f320dfda88af60bb85b6e9b5811a9e6625c47e68
hpps1106 <36985295+hpps1106@users.noreply.github.com>
2023-04-24 17:12:01 +0800

    Update

--- Lab4-BlockRAM/Lab4-2_HDL_Synthesis/hdl/memory_ctrl.v
+++ Lab4-BlockRAM/Lab4-2_HDL_Synthesis/hdl/memory_ctrl.v
@@ -1,31 +1,32 @@
 module	memory_ctrl(
-	input					clk,
-	input					rst_n,
-	input 				cmd_valid,
+	input			clk,
+	input			rst_n,
+	input 			cmd_valid,
 	input 	[7:0]	cmd,
 	input 	[7:0]	data_in,
 	input 	[7:0]	address,
-	output 				cmd_done,
+	output 			cmd_done,
 	output 	[7:0]	data_out
 );
 
-wire 	[1:0]	next_state;
-wire    [1:0]   next_clk_4;
-wire            clk_4_f;
-wire				write_enable;
-wire	[7:0]	data_in_to_mem;
+wire  [1:0]  next_state;
+wire  [1:0]  next_clk_4;
+wire         clk_4_f;
+wire 	     write_enable;
+wire  [7:0]  data_in_to_mem;
 
-reg 	[1:0]	state;		//0 : wait_cmd	1 : read 2 : wrtie_edge 3 : idle
-reg     [1:0]   clk_4;
+reg   [1:0]  state;		//0 : wait_cmd	1 : read 2 : wrtie_edge 3 : idle
+reg   [1:0]  clk_4;
 
-assign			cmd_done = ((state == 2'd0) || (state == 2'd3))? 1'd1 : 1'd0;
-assign			next_state = (state == 2'd0)? {cmd_valid & cmd[1], cmd_valid & cmd[0]} :
-												 (state == 2'd1)? 2'd3 :
-												 (state == 2'd2)? 2'd3 : {cmd_valid, cmd_valid};
-assign			write_enable = (state == 2'd2)? 1'd1 : 1'd0;
-assign			data_in_to_mem = (state == 2'd2)? data_in : 8'd0;
-assign          next_clk_4 = (clk_4 == 2'd3)? 2'd0 : clk_4 + 2'd1;
-assign          clk_4_f = clk_4[1];
+assign cmd_done = ((state == 2'd0) || (state == 2'd3))? 1'd1 : 1'd0;
+assign next_state = (state == 2'd0) ? {cmd_valid & cmd[1], cmd_valid & cmd[0]} :
+					(state == 2'd1) ? 2'd3 :
+					(state == 2'd2) ? 2'd3 : {cmd_valid, cmd_valid};
+
+assign write_enable = (state == 2'd2)? 1'd1 : 1'd0;
+assign data_in_to_mem = (state == 2'd2)? data_in : 8'd0;
+assign next_clk_4 = (clk_4 == 2'd3)? 2'd0 : clk_4 + 2'd1;
+assign clk_4_f = clk_4[1];
 
 mem256X8 mem_i(
 	.clk(clk),
@@ -38,11 +39,11 @@ always @(negedge clk or negedge rst_n)
 begin
 	if(!rst_n)
 	begin
-		clk_4	<= 2'd0;
+		clk_4 <= 2'd0;
 	end
 	else
 	begin
-		clk_4	<= next_clk_4;
+		clk_4 <= next_clk_4;
 	end
 end
 
@@ -50,11 +51,11 @@ always @(negedge clk_4_f or negedge rst_n)
 begin
 	if(!rst_n)
 	begin
-		state	<= 2'd0;
+		state <= 2'd0;
 	end
 	else
 	begin
-		state	<= next_state;
+		state <= next_state;
 	end
 end
 
